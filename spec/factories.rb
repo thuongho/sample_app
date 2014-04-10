ed087866adf1496c0c3c34ddb18d144c1a36cea4
Thuong Ho <thuong.t.ho@gmail.com>
2014-04-10 00:50:36 -0700

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
   factory :user do
-    name  "Sam Michel"
-    email  "sam.michel@example.com"
+    # name  "Sam Michel"
+    # email  "sam.michel@example.com"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password  "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
